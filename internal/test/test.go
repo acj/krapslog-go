beb45003531099e1babfdd7613e39447a22d03a6
Adam Jensen <acjensen@gmail.com>
2020-07-03 20:24:19 -0400

    Remove unused function

--- internal/test/test.go
+++ internal/test/test.go
@@ -10,14 +10,6 @@ func RepeatFloat(num float64, count int) []float64 {
 	return a
 }
 
-func RepeatInt64(num int64, count int) []int64 {
-	a := make([]int64, count, count)
-	for idx := range a {
-		a[idx] = num
-	}
-	return a
-}
-
 func RepeatTime(t time.Time, count int) []time.Time {
 	a := make([]time.Time, count, count)
 	for idx := range a {
